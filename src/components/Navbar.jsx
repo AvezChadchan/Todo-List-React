9181851ef4657f1bedc150ff3fa6f03cbb181ea8
Avez Chadchan <avezchadchan@gmail.com>
2025-08-25 23:42:29 +0530

    small changes in navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='flex justify-between bg-gray-900 text-white py-2'>
+    <nav className='flex justify-around bg-gray-900 text-white py-2'>
         <div className="logo cursor-default">
             <span className="font-bold text-xl mx-8">iTask</span>
         </div>
