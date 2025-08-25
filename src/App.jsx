918e2be2b7a69ce05e28342af6c41e9476527a4c
Avez Chadchan <avezchadchan@gmail.com>
2025-08-25 23:32:19 +0530

    project done

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,37 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Navbar from './components/Navbar'
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid'
+import { MdEdit, MdDelete } from "react-icons/md";
+import { FaEdit } from "react-icons/fa";
 
 function App() {
-  const [todo, setTodo] = useState("");
+  const [todo, setTodo] = useState("")
   const [todos, setTodos] = useState([]);
+  const [showFinished, setshowFinished] = useState(true)
+
+  useEffect(() => {
+    let todoString = localStorage.getItem("todos")
+    if (todoString) {
+      let todos = JSON.parse(localStorage.getItem("todos"))
+      setTodos(todos)
+    }
+  }, [])
+
+
+  const saveToLS = () => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }
+
   const handleAdd = () => {
     setTodos([...todos, { id: uuidv4(), todo, isCompleted: false }]);
-    console.log(todos);
     setTodo("");
-  };
-  const handleChange = (e) => {
-    setTodo(e.target.value)
+    saveToLS();
   }
+
+  const handleChange = (e) => {
+    setTodo(e.target.value);
+  }
+
   const handleCheck = (e) => {
     let id = e.target.name;
     let index = todos.findIndex(
@@ -22,53 +41,61 @@ function App() {
     let newTodos = [...todos];
     newTodos[index].isCompleted = !newTodos[index].isCompleted;
     setTodos(newTodos);
+    saveToLS();
   }
+
   const handleEdit = (e, id) => {
     let editTodo = todos.filter(
-      item => {
-        return item.id === id;
-      });
-      // console.log(editTodo);
-      
+      item => item.id === id
+    );
     setTodo(editTodo[0].todo)
-    handleDelete(id);
-  };
-  const handleDelete = (id) => {
-    // console.log(id);
+    handleDelete(e, id)
+    saveToLS()
+  }
+
+  const handleDelete = (e, id) => {
     let newToods = todos.filter(
       item => {
-        return item.id !== id;
+        return item.id !== id
       });
     setTodos(newToods)
-  };
+    saveToLS()
+  }
 
+  const toggleFinished = () => {
+    setshowFinished(!showFinished)
+  }
   return (
     <>
       <Navbar />
-      <div className="container mx-auto my-5 rounded-xl p-5 bg-blue-200 min-h-[80vh] shadow-2xl shadow-blue-950">
-        <div className="addTodo my-4 mx-2">
+      <div className="container mx-3 md:mx-auto my-5 rounded-xl p-5 bg-blue-200 min-h-[80vh] shadow-2xl shadow-blue-950 md:w-[38%]">
+        <h1 className='font-bold text-center text-2xl'>iTask - Manage Your todos at one place</h1>
+        <div className="addTodo my-4 mx-2 flex flex-col gap-4">
           <h2 className='text-lg font-bold'>Add a Todo</h2>
-          <input type="text" onChange={handleChange} value={todo} className='bg-white rounded-md w-1/2' />
-          <button onClick={handleAdd} className='bg-blue-900 hover:bg-blue-950 text-sm font-bold text-white p-3 py-1 rounded-md mx-6'>Add</button>
+          {/* <div className="flex"> */}
+
+          <input type="text" onChange={handleChange} value={todo} className='bg-blue-50 rounded-lg w-full px-3 py-1' />
+          <button onClick={handleAdd} disabled={todo.length <= 2} className='bg-blue-900 hover:bg-blue-950 text-sm font-bold text-white p-3 py-1 rounded-lg disabled:bg-blue-500 cursor-pointer '>Add</button>
+          {/* </div> */}
         </div>
-        <h2 className='text-lg font-bold'>Your Todos</h2>
+        <input type="checkbox" onChange={toggleFinished} checked={showFinished} className='my-4 ' /> Show Finished
+        <hr className='mx-auto w-[85%] my-1 opacity-30' />
+        <h2 className='text-lg font-bold my-2'>Your Todos</h2>
         <div className="todos">
           {todos.length === 0 && <div className='m-5'>No Todo's to Display</div>}
           {todos.map(
-            (item) => (
-              <div className="todo flex w-1/4 justify-between my-3" key={item.id}>
-
+            (item) => {
+              return (showFinished || !item.isCompleted) && <div className="todo flex justify-between my-3" key={item.id}>
                 <div className='flex gap-5'>
-                  <input type="checkbox" onChange={handleCheck} value={item.isCompleted} name={item.id} />
+                  <input type="checkbox" onChange={handleCheck} checked={item.isCompleted} name={item.id} />
                   <div className={item.isCompleted ? "line-through" : ""}>{item.todo}</div>
-
                 </div>
-                <div className="buttons">
-                  <button onClick={(e) => { handleEdit(e, item.id) }} className='bg-blue-900 hover:bg-blue-950 text-sm font-bold text-white p-3 py-1 rounded-md mx-1'>Edit</button>
-                  <button onClick={() => { handleDelete(item.id) }} className='bg-blue-900 hover:bg-blue-950 text-sm font-bold text-white p-3 py-1 rounded-md mx-1'>Delete</button>
+                <div className="buttons flex h-full">
+                  <button onClick={(e) => { handleEdit(e, item.id) }} className='bg-blue-900 hover:bg-blue-950 text-sm font-bold text-white p-3 py-1 rounded-md mx-1 cursor-pointer'><FaEdit /></button>
+                  <button onClick={(e) => { handleDelete(e, item.id) }} className='bg-blue-900 hover:bg-blue-950 text-sm font-bold text-white p-3 py-1 rounded-md mx-1 cursor-pointer'><MdDelete /></button>
                 </div>
               </div>
-            ))}
+            })}
 
         </div>
       </div>
